9407b9f2c4b5570f325273bb8fe2b06f489f5899
Ludovit <ludovitkramar@proton.me>
2026-02-25 18:30:05 +0100

    add ftypFACE to mp4 signatures

--- src/signatures.rs
+++ src/signatures.rs
@@ -635,6 +635,7 @@ signatures! {
 
     format = Mpeg4Part14
     value = b"ftypARRI" offset = 4
+    value = b"ftypFACE" offset = 4
     value = b"ftypMSNV" offset = 4
     value = b"ftypNDAS" offset = 4
     value = b"ftypNDSC" offset = 4
